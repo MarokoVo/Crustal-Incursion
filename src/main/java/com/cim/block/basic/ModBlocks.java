ae3b344349d3446cd52388cfddfbfbf1dd4184fd
H-y-p-e-r-i-O <utkonos.na.tolchke@gmail.com>
2026-03-13 08:18:01 +0100

    Double registration fix (AGAIN)

--- src/main/java/com/cim/block/basic/ModBlocks.java
+++ src/main/java/com/cim/block/basic/ModBlocks.java
@@ -142,15 +142,7 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(0.5F, 6.0F).sound(SoundType.NETHERITE_BLOCK).requiresCorrectToolForDrops()));
 
-
-
-
-
-    public static final RegistryObject<Block> DECO_LEAD = registerBlock("deco_lead",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(0.5F, 6.0F).sound(SoundType.NETHERITE_BLOCK).requiresCorrectToolForDrops()));
-
-
+    
 
     //СТУПЕНИ И ПОЛУБЛОКИ
     public static final RegistryObject<StairBlock> CONCRETE_STAIRS = registerBlock("concrete_stairs",
